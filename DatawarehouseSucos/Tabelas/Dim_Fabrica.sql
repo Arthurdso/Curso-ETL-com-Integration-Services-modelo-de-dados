571ffa29c500c849e044154ff9f38b2ff7aa0510
Arthur Oliveira <arthurdso96@gmail.com>
2022-03-31 15:15:34 -0300

    Initial commit

---
+++ DatawarehouseSucos/Tabelas/Dim_Fabrica.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Dim_Fabrica]
+(
+	[Cod_Fabrica] NVARCHAR(50) NOT NULL , 
+    [Desc_Fabrica] NVARCHAR(200) NULL, 
+    CONSTRAINT [PK_Dim_Fabrica] PRIMARY KEY ([Cod_Fabrica])
+)
